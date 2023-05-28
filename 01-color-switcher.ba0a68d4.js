54c94bf17a176c2b7c397b5f423b0d38c8e5e5d4
maks-vynnychenko <113777935+maks-vynnychenko@users.noreply.github.com>
2023-05-28 12:10:44 +0000

    Deploying to gh-pages from @ maks-vynnychenko/goit-js-hw-09@7e49227110938117c7936ed3c712d2b8aafc0013 🚀

--- 01-color-switcher.ba0a68d4.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(e){timerId=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.disabled=!0})),e.addEventListener("click",(function(e){clearInterval(timerId),t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.ba0a68d4.js.map
